037aa8f21f0645464f863ff435b07e41c2c5c238
Vlad Shuman <shumanvlad56@gmail.com>
2024-06-28 00:16:23 +0300

    Beginning of Qt3

---
+++ Qt3/Iterator.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "MyList.h"
+class MyIterator {
+public:
+    MyIterator() :ptr(nullptr) {};
+    MyIterator(SElement* ptr_) :ptr(ptr_) {};
+    ~MyIterator() = default;// dont know what to do with it
+
+    MyIterator First()
+    {
+        //ptr = first
+    }
+    MyIterator Next() 
+    {
+        ptr = ptr->next;
+        return *this;
+    }
+    bool isDone()
+    {
+        return ptr == nullptr; // I cmp ptr with nullptr because of the next element of the last element of list is equal nullptr ( how i belive)
+    }
+    int CurrentItem()
+    {
+        return ptr->data;
+    }
+    
+    bool operator!=(const MyIterator& it) const
+    {
+        return ptr != it.ptr;
+    }
+    bool operator==(const MyIterator& it) const
+    {
+        return ptr == it.ptr;
+    }
+    
+private:
+    SElement* ptr;
+};
+
+MyIterator Nulliterator(nullptr);
---
+++ ReadMe.txt
@@ -0,0 +1,2 @@
+the size of .vs folder is too big.
+that's why i'm ignoring it
